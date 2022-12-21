3b1d4467becd1af41856c45d9070f476053e550b
evgeny_sem163 <evg82tol@gmail.com>
2022-12-21 21:58:41 +0300

    домашнее задание с 1 по 4 номера. (количество бутылок)

---
+++ src/Main.java
@@ -0,0 +1,58 @@
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+      int numberOfPears = 15;
+      byte ram = 127;
+      short fixingBolt = 150;
+      long hugeNumber = 335000;
+      float bigWeight = 180.5f;
+      double km = 120.4;
+
+        System.out.println("Значение переменной  numberOfPears  с типом int равно " + numberOfPears);
+        System.out.println("Значение переменной ram с типом byte равно " + ram );
+        System.out.println("Значение переменной fixingBolt с типом short равно " + fixingBolt);
+        System.out.println("Значение переменной hugeNumber с типом long равно " + hugeNumber);
+        System.out.println("Значение переменной bigWeight с типом float равно " + bigWeight);
+        System.out.println("Значение переменной km с типом double равно " + km);
+
+        float circle = 27.12f;
+        long ship = 98767896549L;
+        double money = 2.786;
+        int parrot = 569;
+        int quail = - 159;
+        int pen = 27897;
+        short window = 69;
+
+        int lyudmilaPavlovna = 23;
+        int annaSergeyevna = 27;
+        int еkaterinaAndreevna = 30;
+        int sheet = 480;
+        int total = lyudmilaPavlovna + annaSergeyevna + еkaterinaAndreevna / sheet;
+      System.out.println( " на каждого ученика расчитано " +  total + " листов бумаги ");
+
+        int bottle = 16;
+        int minute = 2;
+        int oneMinute = bottle / minute;
+//      System.out.println(oneMinute);
+       int twentyTime = 20;
+      System.out.println( " производительность машины за 20 минут " + oneMinute * twentyTime);
+       int day = 1440;
+      System.out.println(" производительность машины за сутки " + oneMinute * day );
+      int threeDays = 3420;
+      System.out.println(" производительность машины за трое суток " + oneMinute * threeDays );
+      int month = 35340;
+      System.out.println(" производительность машины за месяц " + oneMinute * month);
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
